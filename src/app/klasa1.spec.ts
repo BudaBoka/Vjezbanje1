881f06f2a24b8917014f394088b5c6535c814f9c
BudaBoka <budimirivan0@gmail.com>
2019-09-13 12:29:18 +0200

    Add Vjezbanje 1

---
+++ src/app/klasa1.spec.ts
@@ -0,0 +1,7 @@
+import { Klasa1 } from './klasa1';
+
+describe('Klasa1', () => {
+  it('should create an instance', () => {
+    expect(new Klasa1()).toBeTruthy();
+  });
+});
---
+++ src/app/klasa1.ts
@@ -0,0 +1,2 @@
+export class Klasa1 {
+}
